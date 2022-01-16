65ceedc37dc841e155ac17b23e315892fff7b88f
Eric Faris <ericfaris@gmail.com>
2022-01-16 15:40:20 -0500

    changing to port 7080

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const PORT = 6080;
+const PORT = 7080;
 const v1Router = require('./routers/api.v1')
 const {VPSDataHelper} = require('./helpers/VPSDataHelper')
 
